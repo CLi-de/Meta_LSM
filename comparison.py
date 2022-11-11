af18345fa132b608a157f465dd91eeb08220d336
CLi-de <browsercl@gmail.com>
2022-11-11 11:12:08 +0800

    move draw_ROC to figure_v2.py

--- comparison.py
+++ comparison.py
@@ -1,19 +1,15 @@
 # -*- coding:utf-8 -*-
-from sklearn import svm
-from sklearn import metrics
+
 import numpy as np
 import pandas as pd
 
+from sklearn import metrics
+from sklearn import svm
 from sklearn.neural_network import MLPClassifier
-import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics._classification import accuracy_score
 
-import matplotlib as mpl
-from sklearn.model_selection import cross_val_score
-from sklearn.datasets import make_blobs
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
 def cal_measure(pred, y_test):
     TP = ((pred == 1) * (y_test == 1)).astype(int).sum()
@@ -42,13 +38,6 @@ def SVM_compare(x_train, y_train, x_test, y_test):
     # Precision, Recall, F1-score
     cal_measure(predict_results, y_test)
 
-    # # train AUROC
-    # fpr1, tpr1, threshold1 = metrics.roc_curve(y_train, predict_results1, pos_label=1)
-    # print(metrics.auc(fpr1, tpr1))
-    # # Test AUROC
-    # fpr, tpr, threshold = metrics.roc_curve(y_test, predict_results, pos_label=1)
-    # print(metrics.auc(fpr, tpr))
-
     """LSM prediction"""
     grid_f = np.loadtxt('./src_data/grid_samples_HK.csv', dtype=str, delimiter=",",
                         encoding='UTF-8')
@@ -62,7 +51,7 @@ def SVM_compare(x_train, y_train, x_test, y_test):
     writer = pd.ExcelWriter('./tmp/SVM_prediction_HK.xlsx')
     data_df.to_excel(writer, 'page_1', float_format='%.5f')
     writer.close()
-    print('done SVM LSM prediction!')
+    print('done SVM LSM prediction! \n')
 
 
 def ANN_compare(x_train, y_train, x_test, y_test):
@@ -72,37 +61,12 @@ def ANN_compare(x_train, y_train, x_test, y_test):
                           batch_size=32, max_iter=1000)
     model.fit(x_train, y_train)
     predict1 = model.predict(x_train)
-    print('Done.\n Train Accuracy: %f' % accuracy_score(y_train, predict1))  # 奉节，在0.82 - 0.90；
+    print('Train Accuracy: %f' % accuracy_score(y_train, predict1))  # 奉节，在0.82 - 0.90；
     # test
     predict = model.predict(x_test)
-    print('Done.\n Test Accuracy: %f' % accuracy_score(y_test, predict))  # 奉节，在0.72 - 0.76间；涪陵，在0.76-0.81之间
+    print('Test Accuracy: %f' % accuracy_score(y_test, predict))  # 奉节，在0.72 - 0.76间；涪陵，在0.76-0.81之间
     # Precision, Recall, F1-score
     cal_measure(predict, y_test)
-
-    # # train AUROC
-    # fpr1,tpr1,threshold1 = metrics.roc_curve(y_train,predict1,pos_label=1)
-    # print(metrics.auc(fpr1,tpr1))
-    #
-    # # Test AUROC
-    # fpr,tpr,threshold = metrics.roc_curve(y_test,predict,pos_label=1)
-    # print(metrics.auc(fpr,tpr))
-
-    # # Compute test ROC curve and ROC area for each class
-    # fpr, tpr, threshold = metrics.roc_curve(y_test, predict)  ###计算真正率和假正率
-    # roc_auc = metrics.auc(fpr, tpr)  ###计算auc的值
-    # plt.figure()
-    # lw = 2
-    # plt.figure(figsize=(10, 10))
-    # plt.plot(fpr, tpr, color='darkorange',
-    #          lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
-    # plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    # # plt.xlim([0.0, 1.0])
-    # # plt.ylim([0.0, 1.05])
-    # plt.xlabel('False Positive Rate')
-    # plt.ylabel('True Positive Rate')
-    # plt.title('Receiver operating characteristic example')
-    # plt.legend(loc="lower right")
-    # plt.show()
 
     """LSM prediction"""
     grid_f = np.loadtxt('./src_data/grid_samples_HK.csv', dtype=str, delimiter=",",
@@ -117,7 +81,7 @@ def ANN_compare(x_train, y_train, x_test, y_test):
     writer = pd.ExcelWriter('./tmp/MLP_prediction_HK.xlsx')
     data_df.to_excel(writer, 'page_1', float_format='%.5f')
     writer.close()
-    print('done RF LSM prediction!')
+    print('done MLP LSM prediction! \n')
 
 
 def RF_compare(x_train, y_train, x_test, y_test):
@@ -130,9 +94,9 @@ def RF_compare(x_train, y_train, x_test, y_test):
     pred_train = clf.predict(x_train)
     pred_test = clf.predict(x_test)
     # 训练精度
-    print('Done.\ntrain_Accuracy: %f' % accuracy_score(y_train, pred_train))
+    print('train_Accuracy: %f' % accuracy_score(y_train, pred_train))
     # 测试精度
-    print('Done.\ntest_Accuracy: %f' % accuracy_score(y_test, pred_test))  # 0.71 - 0.77
+    print('test_Accuracy: %f' % accuracy_score(y_test, pred_test))  # 0.71 - 0.77
     # pred1 = clf2.predict_proba() # 预测类别概率
     cal_measure(pred_test, y_test)
 
@@ -148,7 +112,7 @@ def RF_compare(x_train, y_train, x_test, y_test):
     writer = pd.ExcelWriter('./tmp/RF_prediction_HK.xlsx')
     data_df.to_excel(writer, 'page_1', float_format='%.5f')
     writer.close()
-    print('done MLP LSM prediction!')
+    print('done RF LSM prediction! \n')
 
 """SVM"""
 # Input data
@@ -166,8 +130,8 @@ x_test = tmp_[int(tmp_.shape[0] / 2):, :-1]  # 加载i行数据部分
 y_test = tmp_[int(tmp_.shape[0] / 2):, -1]  # 加载类别标签部分
 x_test = x_test / x_test.max(axis=0)
 
-# SVM_compare(x_train, y_train, x_test, y_test)
+SVM_compare(x_train, y_train, x_test, y_test)
 
-# ANN_compare(x_train, y_train, x_test, y_test)
+ANN_compare(x_train, y_train, x_test, y_test)
 
 RF_compare(x_train, y_train, x_test, y_test)
