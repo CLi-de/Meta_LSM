974d358d866e8fd66109f0aad9c74155696534f1
CLi-de <browsercl@gmail.com>
2022-12-02 14:27:44 +0800

    GPG keys

--- meta_learner_v2.py
+++ meta_learner_v2.py
@@ -41,7 +41,7 @@ flags.DEFINE_integer('test_update_batch_size', 8,
 flags.DEFINE_integer('metatrain_iterations', 5001, 'number of meta-training iterations.')
 flags.DEFINE_integer('num_updates', 5, 'number of inner gradient updates during training.')
 flags.DEFINE_integer('pretrain_iterations', 0, 'number of pre-training iterations.')
-# flags.DEFINE_integer('num_samples', 18469, 'total number of samples in HK (see samples_HK).')
+# flags.DEFINE_integer('num_samples', 18469, 'total number of samples in HK, see samples_HK.')
 flags.DEFINE_float('update_lr', 1e-2, 'learning rate of single task objective (inner)')  # le-2 is the best
 flags.DEFINE_float('meta_lr', 1e-3, 'the base learning rate of meta objective (outer)')  # le-2 or le-3
 flags.DEFINE_bool('stop_grad', False, 'if True, do not use second derivatives in meta-optimization (for speed)')
